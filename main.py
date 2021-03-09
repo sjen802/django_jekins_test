ea1d4e03187b4fb5fde72055f136dca6d5906e6b
sjen802 <iiiiieeiiiiii@gmail.com>
2021-03-09 20:56:52 +0800

    change amin

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from linebot_ToolsOfYoutuber.wsgi import application
+from djangoproject2.wsgi import application
 
 # App Engine by default looks for a main.py file at the root of the app
 # directory with a WSGI-compatible object called app.
